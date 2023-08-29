f0947d8b348925dbf6a70b5f06a50865f0c53ded
Gisely Farias <giselyfarias7@gmail.com>
2023-08-29 07:51:37 -0300

    Viagens

---
+++ src/Turista.java
@@ -0,0 +1,16 @@
+public class Turista {
+    //Atributos
+    String nome;
+    String cpf;
+
+    //Construtor
+    Turista(String _nome){
+        this.nome = _nome;
+    }
+
+    //Métodos
+    public String viajar(){
+        return "Viajou!!!";
+    }
+
+}
